711e9ee7072f9b8490288c57ab987800825692f7
Yangyutong1 <774201425@qq.com>
2024-12-18 16:29:29 +0800

    流的去重和过滤

--- src/main/java/controller/streamController/streamDemo.java
+++ src/main/java/controller/streamController/streamDemo.java
@@ -4,24 +4,29 @@ import controller.domain.Stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 //采用流的形式，去重打印年龄小于25岁的人名
 
 public class streamDemo {
 
     public static void main(String[] args) {
-        getAuthor().stream().distinct()
-                .filter(age -> age.getAge() < 25)
-                .forEach(name -> System.out.println(name.getName()));
+//        getAuthor().stream().filter(e -> e.getName().length() > 3).forEach(e-> System.out.println(e.getName()));
+//        getAuthor().stream().map(e->e.getName()).forEach(e-> System.out.println(e));
+//        getAuthor().stream().distinct().forEach(e-> System.out.println(e.getName()));
+        
     }
 
     private static List<Stream> getAuthor() {
         List list = new ArrayList<>();
-        Stream stream = new Stream(1, "A", 20, "AAAAAAAAAAAAAAAAA", 11);
-        Stream stream1 = new Stream(2, "B", 22, "BBBBBBBBBBBBBBBBB", 22);
-        Stream stream2 = new Stream(3, "C", 34, "CCCCCCCCCCCCCCCCC", 33);
-        Stream stream3 = new Stream(4, "D", 17, "DDDDDDDDDDDDDDDDDDD", 44);
-        Stream stream4 = new Stream(4, "D", 17, "DDDDDDDDDDDDDDDDDDD", 44);
+        Stream stream = new Stream(1, "测试1", 20, "AAAAAAAAAAAAAAAAA", 11);
+        Stream stream1 = new Stream(2, "测试AA", 22, "BBBBBBBBBBBBBBBBB", 22);
+        Stream stream2 = new Stream(3, "测试ABC", 34, "CCCCCCCCCCCCCCCCC", 33);
+        Stream stream3 = new Stream(4, "测试D", 17, "DDDDDDDDDDDDDDDDDDD", 44);
+        Stream stream4 = new Stream(4, "测试D", 17, "DDDDDDDDDDDDDDDDDDD", 44);
 
         list.add(stream);
         list.add(stream1);
